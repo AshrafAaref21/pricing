d907a603edbc8140305d651a7fe2d14daf896288
Ashraf Aaref <82468597+AshrafAaref21@users.noreply.github.com>
2023-10-11 14:50:03 +0300

    Update target.py

--- target.py
+++ target.py
@@ -2,97 +2,213 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+import os
 
-
-img = Image.open("etman_logo.ico")    
+# open Etman logo file
+img = Image.open("etman_logo.ico")
+img = img.resize((100,100))
+# Set up the page configuration 
 st.set_page_config(
     page_title="Etman Group",
     page_icon=img,
     layout="wide",
     )
 
-st.header('Etman Scotch Pricing\nMade by Eng\\ ***Ashraf Aaref***')
-st.write('-----------------------------------')
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+   st.header('Etman Pricing\nMade by Eng\\ ***Ashraf Aaref***')
+   
+with col5:
+   st.image(img)
+
+st.divider()
 
 
+product_bx = st.selectbox('Choose The Product',['Scotch','Alumnium Foil','Alumnium Tray','x'],index=0)
+st.divider()
 
-sheet_bx = st.selectbox('Select',['Select The Pricing Type','Temporary','Final'],index=0)
+if product_bx == 'Scotch':
 
-if sheet_bx == 'Temporary':
+   # Reading latest Scotch Cost
+   df_data = pd.read_excel(
+      r'data.xlsx',
+      sheet_name='Final',
+      index_col=0
+      )
+   
+   # Reading latest Scotch Margin Profit
+   df_margin = pd.read_excel(
+      r'data.xlsx',
+      sheet_name='Margin',
+      index_col=0
+      )
+   
+   # Reading latest Shrink Data
+   df_shrink = pd.read_excel(
+      r'scotch priceing 9.xlsx',
+      sheet_name= 'تكلفة الفارغ',
+      #index_col=0,
+      header=2,
+      usecols=[0,5,6],
+      names=['width','core_cost','shrink_cost']
+      )
+   df_shrink.width = df_shrink.width.astype(float)
+   df_shrink.set_index('width',inplace=True)
 
-    df = pd.read_excel(
-        r'temporary _grace.xlsx',
-        sheet_name='new',
-        )
+   # Reading the latest Production cost
+   df_production = pd.read_excel(
+      r'overhead.xlsx',
+      sheet_name= 'cost_per_meter',
+      )
+   df_production.set_index('units',inplace=True)
+   
 
-    df.ID = df.ID.astype(int)
-    df = df.set_index('ID')
+   # Creating Radio button for Scotch Types
+   color_option = st.radio('Choose Scotch Color',['Clear','Crystal','Super','Color','Doku','Laser'],index=0)
+
+   st.divider()
+
+   if color_option not in ['Doku', 'Laser']:
+      # Create input for Scotch features
+      micron = st.number_input('Micron', min_value=30, max_value=60, value=45, step=1)
+      width = st.selectbox('Choose The Width in CM (العرض)', df_shrink.index)
+      length = st.number_input('Length in Meter (الطول)', min_value=0,value=15)
+      n_rollers = st.number_input('Rollers Quantity (عدد البكر في الكرتونة)', min_value=1, step=1)
+
+      # Calculate the meter Price
+      kg_price = df_data.loc[color_option.lower(),'price per unit']
+      meter_price = 1.03 * kg_price*(micron*0.98/1000)
+      #Calculate The raw Material cost
+      raw = length * meter_price
+
+      # Calculate the Shrink Cost
+      if length >= 200:
+         shrink = 0
+      else:
+         shrink = df_shrink.loc[width,'shrink_cost']
+      
+      # Calculate the Core Cost
+      core = df_shrink.loc[width,'core_cost']
+
+      # Calculate the Production Cost
+      if color_option in ['Clear', 'Color']:
+         if width == 1.2:
+            production = length * df_production.iloc[0,0]
+         elif width in [1.8,2.3,2.4]:
+            production = (length * width/100) * n_rollers * df_production.iloc[1,0]
+         else:
+            if length > 0 and length <= 15:
+               production = (length * width/100) * n_rollers * df_production.iloc[2,0]
+            elif length > 15 and length <= 40:
+               production = (length * width/100) * n_rollers * df_production.iloc[3,0]
+            elif length > 40 and length <= 60:
+               production = (length * width/100) * n_rollers * df_production.iloc[4,0]
+            elif length > 60 and length <= 100:
+               production = (length * width/100) * n_rollers * df_production.iloc[5,0]
+            elif length > 100 and length <= 150:
+               production = (length * width/100) * n_rollers * df_production.iloc[6,0]
+            elif length > 150 and length <= 250:
+               production = (length * width/100) * n_rollers * df_production.iloc[7,0]
+            elif length > 250:
+               production = (length * width/100) * n_rollers * df_production.iloc[8,0]
+      else:
+         if width == 1.2:
+            production = (length * width/100) * n_rollers * df_production.iloc[0,1]
+         elif width in [1.8,2.3,2.4]:
+            production = (length * width/100) * n_rollers * df_production.iloc[1,1]
+         else:
+            if length > 0 and length <= 15:
+               production = (length * width/100) * n_rollers * df_production.iloc[2,1]
+            elif length > 15 and length <= 40:
+               production = (length * width/100) * n_rollers * df_production.iloc[3,1]
+            elif length > 40 and length <= 60:
+               production = (length * width/100) * n_rollers * df_production.iloc[4,1]
+            elif length > 60 and length <= 100:
+               production = (length * width/100) * n_rollers * df_production.iloc[5,1]
+            elif length > 100 and length <= 150:
+               production = (length * width/100) * n_rollers * df_production.iloc[6,1]
+            elif length > 150 and length <= 250:
+               production = (length * width/100) * n_rollers * df_production.iloc[7,1]
+            elif length > 250:
+               production = (length * width/100) * n_rollers * df_production.iloc[8,1]
 
 
-    goods_cost = st.number_input('Cost of Goods (1 ton)')
+      if n_rollers > 6:
+         TOTAL_COST = meter_price * (length*width/100)*n_rollers + production + n_rollers*(shrink + core) + 15
+      else:
+         TOTAL_COST = meter_price * (length*width/100)*n_rollers + production + n_rollers*(shrink + core)
+
+      btn = st.button('Calculate',type="primary")
+      if btn:
+         st.success(TOTAL_COST * (1 + df_margin.loc[color_option.lower(),'margin']/100))
+         st.snow()
 
 
-    import_slider = st.selectbox('Select Import Container',[40,20])
-    shipping_import = st.number_input('Shipping Cost for Import')
-
-    export_slider = st.selectbox('Select Export Container',[40,20])
-    shipping_export = st.number_input('Shipping Cost for Export')
-
-
-    btn = st.button('Calculate')
-
-
-    if btn:
-        eq = goods_cost + (1.03*shipping_import/df.loc[import_slider,'Quantity']) + shipping_export/(df.loc[export_slider,'Quantity']) + df.loc[export_slider,df.columns[1]] + df.loc[export_slider,df.columns[2]]
-        st.write(f"$  {int(np.ceil(eq))}")  
-
-            # file = 'my_csv.csv'
-
-            # def appendDFToCSV_void(df, csvFilePath, sep=",",encoding='utf-8'):
-            #     try:
-            #         if not os.path.isfile(csvFilePath):
-            #             df.to_csv(csvFilePath, mode='a', index=False, sep=sep)
-            #         elif len(df.columns) != len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns):
-            #             raise Exception("Columns do not match!! Dataframe has " + str(len(df.columns)) + " columns. CSV file has " + str(len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns)) + " columns.")
-            #         elif not (df.columns == pd.read_csv(csvFilePath, nrows=1, sep=sep).columns).all():
-            #             raise Exception("Columns and column order of dataframe and csv file do not match!!")
-            #         else:
-            #             df.to_csv(csvFilePath, mode='a', index=False, sep=sep, header=False)
-            #     except PermissionError:
-            #         pass
-
-            # appendDFToCSV_void(df,file)
-
-            
-if sheet_bx == 'Final':
-
-    df = pd.read_excel(
-    r'temporary _grace.xlsx',
-    sheet_name='new',
-    )
-
-    # st.dataframe(df)
-
-    df.ID = df.ID.astype(int)
-    df = df.set_index('ID')
+   # Doku And Laser
+   else:
+      if color_option == 'Doku':
+         width = st.selectbox('Choose The Width in CM (العرض)', df_shrink.index)
+      else:
+         width = st.selectbox('Choose The Width in CM (العرض)', [1.2,2.3,4.5,4.8])
+      length = st.number_input('Length in Meter (الطول)', min_value=0)
+      n_rollers = st.number_input('Rollers Quantity (عدد البكر في الكرتونة)', min_value=1, step=1)
 
 
-    goods_cost = st.number_input('Cost of Goods (1 ton)')
+      # Calculate the meter Price
+      meter_price = 1.03 * df_data.loc[color_option.lower(),'price per unit']
+      #Calculate The raw Material cost
+      raw = length * meter_price
+
+      # Calculate the Shrink Cost
+      if length >= 200:
+         shrink = 0
+      else:
+         shrink = df_shrink.loc[width,'shrink_cost']
+      
+      # Calculate the Core Cost
+      core = df_shrink.loc[width,'core_cost']
 
 
-    import_slider = st.selectbox('Select Import Container',['1 x 40 ft','2 x 20 ft'])
-    if import_slider == '1 x 40 ft':
-        con_ft = 40
-        n = 1
-    elif import_slider == '2 x 20 ft':
-        con_ft = 20
-        n = 2
-    shipping_import = st.number_input('Shipping Cost')
+      if color_option == ['Doku']:
+         if width == 1.2:
+            production = length * df_production.iloc[0,2]
+         elif width in [1.8,2.3,2.4]:
+            production = (length * width/100) * n_rollers * df_production.iloc[1,2]
+         else:
+            if length > 0 and length <= 15:
+               production = (length * width/100) * n_rollers * df_production.iloc[2,2]
+            elif length > 15 and length <= 40:
+               production = (length * width/100) * n_rollers * df_production.iloc[3,2]
+            elif length > 40 and length <= 60:
+               production = (length * width/100) * n_rollers * df_production.iloc[4,2]
+            elif length > 60 and length <= 100:
+               production = (length * width/100) * n_rollers * df_production.iloc[5,2]
+            elif length > 100 and length <= 150:
+               production = (length * width/100) * n_rollers * df_production.iloc[6,2]
+            elif length > 150 and length <= 250:
+               production = (length * width/100) * n_rollers * df_production.iloc[7,2]
+            elif length > 250:
+               production = (length * width/100) * n_rollers * df_production.iloc[8,2]
 
-    dollar_price = st.number_input('Dollar Price In Egypt',value=41)
+      else:
+         if width == 1.2:
+            production = length * df_production.iloc[0,3]         
+         if width == 2.3:
+            production = length * df_production.iloc[0,3]
+         if width == 4.5:
+            production = length * df_production.iloc[0,3]         
+         if width == 4.8:
+            production = length * df_production.iloc[0,3] 
 
-    btn = st.button('Calculate')
 
-    if btn:
-        eq = dollar_price * goods_cost + dollar_price*(shipping_import/(n * df.loc[con_ft,'Quantity'])) + df.loc[con_ft,'total_final']
-        st.write(f"{int(np.ceil(1.03*eq))} LE")
+      # Total Cost Formula
+      if n_rollers > 6:
+         TOTAL_COST = meter_price * (length*width/100)*n_rollers + production + n_rollers*(shrink + core) + 10
+      else:
+         TOTAL_COST = meter_price * (length*width/100)*n_rollers + production + n_rollers*(shrink + core)
+
+      btn = st.button('Calculate',type="primary")
+      if btn:
+         st.success(TOTAL_COST * (1 + df_margin.loc[color_option.lower(),'margin']/100))
+         st.snow()
